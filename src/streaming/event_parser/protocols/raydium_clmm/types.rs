ae26d12a49a2dfe1ef65c2570b34dc8adf9f2635
home <923092666@qq.com>
2025-11-18 22:42:20 +0800

    初步添加raydium clmm

--- src/streaming/event_parser/protocols/raydium_clmm/types.rs
+++ src/streaming/event_parser/protocols/raydium_clmm/types.rs
@@ -1,5 +1,6 @@
 use borsh::BorshDeserialize;
 use serde::{Deserialize, Serialize};
+use solana_program::account_info::AccountInfo;
 use solana_sdk::pubkey::Pubkey;
 
 use crate::streaming::{
@@ -163,6 +164,107 @@ pub struct TickState {
     pub padding: [u32; 13],
 }
 
+impl TickState {
+    pub const LEN: usize = 4 + 16 + 16 + 16 + 16 + 16 * REWARD_NUM + 16 + 16 + 8 + 8 + 4;
+
+    pub fn initialize(&mut self, tick: i32, tick_spacing: u16) -> Result<()> {
+        if TickState::check_is_out_of_boundary(tick) {
+            return err!(ErrorCode::InvalidTickIndex);
+        }
+        require!(
+            tick % i32::from(tick_spacing) == 0,
+            ErrorCode::TickAndSpacingNotMatch
+        );
+        self.tick = tick;
+        Ok(())
+    }
+    /// Updates a tick and returns true if the tick was flipped from initialized to uninitialized
+    pub fn update(
+        &mut self,
+        tick_current: i32,
+        liquidity_delta: i128,
+        fee_growth_global_0_x64: u128,
+        fee_growth_global_1_x64: u128,
+        upper: bool,
+        reward_infos: &[RewardInfo; REWARD_NUM],
+    ) -> Result<bool> {
+        let liquidity_gross_before = self.liquidity_gross;
+        let liquidity_gross_after =
+            liquidity_math::add_delta(liquidity_gross_before, liquidity_delta)?;
+
+        // Either liquidity_gross_after becomes 0 (uninitialized) XOR liquidity_gross_before
+        // was zero (initialized)
+        let flipped = (liquidity_gross_after == 0) != (liquidity_gross_before == 0);
+        if liquidity_gross_before == 0 {
+            // by convention, we assume that all growth before a tick was initialized happened _below_ the tick
+            if self.tick <= tick_current {
+                self.fee_growth_outside_0_x64 = fee_growth_global_0_x64;
+                self.fee_growth_outside_1_x64 = fee_growth_global_1_x64;
+                self.reward_growths_outside_x64 = RewardInfo::get_reward_growths(reward_infos);
+            }
+        }
+
+        self.liquidity_gross = liquidity_gross_after;
+
+        // when the lower (upper) tick is crossed left to right (right to left),
+        // liquidity must be added (removed)
+        self.liquidity_net = if upper {
+            self.liquidity_net.checked_sub(liquidity_delta)
+        } else {
+            self.liquidity_net.checked_add(liquidity_delta)
+        }
+            .unwrap();
+        Ok(flipped)
+    }
+
+    /// Transitions to the current tick as needed by price movement, returning the amount of liquidity
+    /// added (subtracted) when tick is crossed from left to right (right to left)
+    pub fn cross(
+        &mut self,
+        fee_growth_global_0_x64: u128,
+        fee_growth_global_1_x64: u128,
+        reward_infos: &[RewardInfo; REWARD_NUM],
+    ) -> i128 {
+        self.fee_growth_outside_0_x64 = fee_growth_global_0_x64
+            .checked_sub(self.fee_growth_outside_0_x64)
+            .unwrap();
+        self.fee_growth_outside_1_x64 = fee_growth_global_1_x64
+            .checked_sub(self.fee_growth_outside_1_x64)
+            .unwrap();
+
+        for i in 0..REWARD_NUM {
+            if !reward_infos[i].initialized() {
+                continue;
+            }
+
+            self.reward_growths_outside_x64[i] = reward_infos[i]
+                .reward_growth_global_x64
+                .checked_sub(self.reward_growths_outside_x64[i])
+                .unwrap();
+        }
+
+        self.liquidity_net
+    }
+
+    pub fn clear(&mut self) {
+        self.liquidity_net = 0;
+        self.liquidity_gross = 0;
+        self.fee_growth_outside_0_x64 = 0;
+        self.fee_growth_outside_1_x64 = 0;
+        self.reward_growths_outside_x64 = [0; REWARD_NUM];
+    }
+
+    pub fn is_initialized(self) -> bool {
+        self.liquidity_gross != 0
+    }
+
+    /// Common checks for a valid tick input.
+    /// A tick is valid if it lies within tick boundaries
+    pub fn check_is_out_of_boundary(tick: i32) -> bool {
+        tick < tick_math::MIN_TICK || tick > tick_math::MAX_TICK
+    }
+}
+
 impl Default for TickState {
     fn default() -> Self {
         Self {
@@ -177,6 +279,10 @@ impl Default for TickState {
     }
 }
 
+pub const TICK_ARRAY_SEED: &str = "tick_array";
+pub const TICK_ARRAY_SIZE_USIZE: usize = 60;
+pub const TICK_ARRAY_SIZE: i32 = 60;
+
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, BorshDeserialize)]
 pub struct TickArrayState {
     pub pool_id: Pubkey,
@@ -189,15 +295,240 @@ pub struct TickArrayState {
     pub padding: [u8; 107],
 }
 
+impl TickArrayState {
+    pub const LEN: usize = 8 + 32 + 4 + TickState::LEN * TICK_ARRAY_SIZE_USIZE + 1 + 115;
+
+    pub fn key(&self) -> Pubkey {
+        Pubkey::find_program_address(
+            &[
+                TICK_ARRAY_SEED.as_bytes(),
+                self.pool_id.as_ref(),
+                &self.start_tick_index.to_be_bytes(),
+            ],
+            &crate::id(),
+        )
+            .0
+    }
+    /// Load a TickArrayState of type AccountLoader from tickarray account info, if tickarray account does not exist, then create it.
+    pub fn get_or_create_tick_array<'info>(
+        payer: AccountInfo<'info>,
+        tick_array_account_info: AccountInfo<'info>,
+        system_program: AccountInfo<'info>,
+        pool_state_loader: &AccountLoader<'info, PoolState>,
+        tick_array_start_index: i32,
+        tick_spacing: u16,
+    ) -> Result<AccountLoad<'info, TickArrayState>> {
+        require!(
+            TickArrayState::check_is_valid_start_index(tick_array_start_index, tick_spacing),
+            ErrorCode::InvalidTickIndex
+        );
+
+        let tick_array_state = if tick_array_account_info.owner == &system_program::ID {
+            let (expect_pda_address, bump) = Pubkey::find_program_address(
+                &[
+                    TICK_ARRAY_SEED.as_bytes(),
+                    pool_state_loader.key().as_ref(),
+                    &tick_array_start_index.to_be_bytes(),
+                ],
+                &crate::id(),
+            );
+            require_keys_eq!(expect_pda_address, tick_array_account_info.key());
+            create_or_allocate_account(
+                &crate::id(),
+                payer,
+                system_program,
+                tick_array_account_info.clone(),
+                &[
+                    TICK_ARRAY_SEED.as_bytes(),
+                    pool_state_loader.key().as_ref(),
+                    &tick_array_start_index.to_be_bytes(),
+                    &[bump],
+                ],
+                TickArrayState::LEN,
+            )?;
+            let tick_array_state_loader = AccountLoad::<TickArrayState>::try_from_unchecked(
+                &crate::id(),
+                &tick_array_account_info,
+            )?;
+            {
+                let mut tick_array_account = tick_array_state_loader.load_init()?;
+                tick_array_account.initialize(
+                    tick_array_start_index,
+                    tick_spacing,
+                    pool_state_loader.key(),
+                )?;
+            }
+            tick_array_state_loader
+        } else {
+            AccountLoad::<TickArrayState>::try_from(&tick_array_account_info)?
+        };
+        Ok(tick_array_state)
+    }
+
+    /**
+     * Initialize only can be called when first created
+     */
+    pub fn initialize(
+        &mut self,
+        start_index: i32,
+        tick_spacing: u16,
+        pool_key: Pubkey,
+    ) -> Result<()> {
+        TickArrayState::check_is_valid_start_index(start_index, tick_spacing);
+        self.start_tick_index = start_index;
+        self.pool_id = pool_key;
+        self.recent_epoch = get_recent_epoch()?;
+        Ok(())
+    }
+
+    pub fn update_initialized_tick_count(&mut self, add: bool) -> Result<()> {
+        if add {
+            self.initialized_tick_count += 1;
+        } else {
+            self.initialized_tick_count -= 1;
+        }
+        Ok(())
+    }
+
+    pub fn get_tick_state_mut(
+        &mut self,
+        tick_index: i32,
+        tick_spacing: u16,
+    ) -> Result<&mut TickState> {
+        let offset_in_array = self.get_tick_offset_in_array(tick_index, tick_spacing)?;
+        Ok(&mut self.ticks[offset_in_array])
+    }
+
+    pub fn update_tick_state(
+        &mut self,
+        tick_index: i32,
+        tick_spacing: u16,
+        tick_state: TickState,
+    ) -> Result<()> {
+        let offset_in_array = self.get_tick_offset_in_array(tick_index, tick_spacing)?;
+        self.ticks[offset_in_array] = tick_state;
+        self.recent_epoch = get_recent_epoch()?;
+        Ok(())
+    }
+
+    /// Get tick's offset in current tick array, tick must be include in tick array， otherwise throw an error
+    fn get_tick_offset_in_array(self, tick_index: i32, tick_spacing: u16) -> Result<usize> {
+        let start_tick_index = TickArrayState::get_array_start_index(tick_index, tick_spacing);
+        require_eq!(
+            start_tick_index,
+            self.start_tick_index,
+            ErrorCode::InvalidTickArray
+        );
+        let offset_in_array =
+            ((tick_index - self.start_tick_index) / i32::from(tick_spacing)) as usize;
+        Ok(offset_in_array)
+    }
+
+    /// Base on swap directioin, return the first initialized tick in the tick array.
+    pub fn first_initialized_tick(&mut self, zero_for_one: bool) -> Result<&mut TickState> {
+        if zero_for_one {
+            let mut i = TICK_ARRAY_SIZE - 1;
+            while i >= 0 {
+                if self.ticks[i as usize].is_initialized() {
+                    return Ok(self.ticks.get_mut(i as usize).unwrap());
+                }
+                i = i - 1;
+            }
+        } else {
+            let mut i = 0;
+            while i < TICK_ARRAY_SIZE_USIZE {
+                if self.ticks[i].is_initialized() {
+                    return Ok(self.ticks.get_mut(i).unwrap());
+                }
+                i = i + 1;
+            }
+        }
+        err!(ErrorCode::InvalidTickArray)
+    }
+
+    /// Get next initialized tick in tick array, `current_tick_index` can be any tick index, in other words, `current_tick_index` not exactly a point in the tickarray,
+    /// and current_tick_index % tick_spacing maybe not equal zero.
+    /// If price move to left tick <= current_tick_index, or to right tick > current_tick_index
+    pub fn next_initialized_tick(
+        &mut self,
+        current_tick_index: i32,
+        tick_spacing: u16,
+        zero_for_one: bool,
+    ) -> Result<Option<&mut TickState>> {
+        let current_tick_array_start_index =
+            TickArrayState::get_array_start_index(current_tick_index, tick_spacing);
+        if current_tick_array_start_index != self.start_tick_index {
+            return Ok(None);
+        }
+        let mut offset_in_array =
+            (current_tick_index - self.start_tick_index) / i32::from(tick_spacing);
+
+        if zero_for_one {
+            while offset_in_array >= 0 {
+                if self.ticks[offset_in_array as usize].is_initialized() {
+                    return Ok(self.ticks.get_mut(offset_in_array as usize));
+                }
+                offset_in_array = offset_in_array - 1;
+            }
+        } else {
+            offset_in_array = offset_in_array + 1;
+            while offset_in_array < TICK_ARRAY_SIZE {
+                if self.ticks[offset_in_array as usize].is_initialized() {
+                    return Ok(self.ticks.get_mut(offset_in_array as usize));
+                }
+                offset_in_array = offset_in_array + 1;
+            }
+        }
+        Ok(None)
+    }
+
+    /// Base on swap directioin, return the next tick array start index.
+    pub fn next_tick_arrary_start_index(&self, tick_spacing: u16, zero_for_one: bool) -> i32 {
+        let ticks_in_array = TICK_ARRAY_SIZE * i32::from(tick_spacing);
+        if zero_for_one {
+            self.start_tick_index - ticks_in_array
+        } else {
+            self.start_tick_index + ticks_in_array
+        }
+    }
+
+    /// Input an arbitrary tick_index, output the start_index of the tick_array it sits on
+    pub fn get_array_start_index(tick_index: i32, tick_spacing: u16) -> i32 {
+        let ticks_in_array = TickArrayState::tick_count(tick_spacing);
+        let mut start = tick_index / ticks_in_array;
+        if tick_index < 0 && tick_index % ticks_in_array != 0 {
+            start = start - 1
+        }
+        start * ticks_in_array
+    }
+
+    pub fn check_is_valid_start_index(tick_index: i32, tick_spacing: u16) -> bool {
+        if TickState::check_is_out_of_boundary(tick_index) {
+            if tick_index > tick_math::MAX_TICK {
+                return false;
+            }
+            let min_start_index =
+                TickArrayState::get_array_start_index(tick_math::MIN_TICK, tick_spacing);
+            return tick_index == min_start_index;
+        }
+        tick_index % TickArrayState::tick_count(tick_spacing) == 0
+    }
+
+    pub fn tick_count(tick_spacing: u16) -> i32 {
+        TICK_ARRAY_SIZE * i32::from(tick_spacing)
+    }
+}
+
 impl Default for TickArrayState {
-    fn default() -> Self {
-        Self {
+    #[inline]
+    fn default() -> TickArrayState {
+        TickArrayState {
             pool_id: Pubkey::default(),
+            ticks: [TickState::default(); TICK_ARRAY_SIZE_USIZE],
             start_tick_index: 0,
-            ticks: core::array::from_fn(|_| TickState::default()),
             initialized_tick_count: 0,
             recent_epoch: 0,
-            padding: [0u8; 107],
+            padding: [0; 107],
         }
     }
 }
