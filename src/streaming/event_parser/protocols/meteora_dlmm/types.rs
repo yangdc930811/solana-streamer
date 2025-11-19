54c9d83ebb731736794e331d59b2c957178cc94b
yangdongcheng <yangdc@lemjoy.com>
2025-11-19 17:58:28 +0800

    添加meteora dlmm解析BinArrayBitmapExtension(可能用不上)

--- src/streaming/event_parser/protocols/meteora_dlmm/types.rs
+++ src/streaming/event_parser/protocols/meteora_dlmm/types.rs
@@ -122,3 +122,23 @@ pub fn pool_parser(account: &AccountPretty, mut metadata: EventMetadata) -> Opti
         None
     }
 }
+
+#[derive(Clone, Debug, Default, PartialEq, Eq, Serialize, Deserialize, BorshDeserialize, Copy)]
+pub struct BinArrayBitmapExtension {
+    pub lb_pair: Pubkey,
+
+    /// Packed initialized bin array state for start_bin_index is positive
+    pub positive_bin_array_bitmap: [[u64; 8]; 12],
+
+    /// Packed initialized bin array state for start_bin_index is negative
+    pub negative_bin_array_bitmap: [[u64; 8]; 12],
+}
+
+pub const BIN_ARRAY_BITMAP_EXTENSION_SIZE: usize = 1568;
+
+pub fn direct_bin_array_bitmap_extension_decode(data: &[u8]) -> Option<BinArrayBitmapExtension> {
+    if data.len() < BIN_ARRAY_BITMAP_EXTENSION_SIZE + 8 {
+        return None;
+    }
+    borsh::from_slice::<BinArrayBitmapExtension>(&data[8..BIN_ARRAY_BITMAP_EXTENSION_SIZE + 8]).ok()
+}
